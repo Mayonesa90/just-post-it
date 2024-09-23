341d20fbfc53a2f2c595c1c52d7ea2aceadfe993
Maya Arzapalo Björklund <mayonaas@hotmail.com>
2024-09-23 19:13:07 +0200

    added try catch to put function and added error handling

--- backend/just-post-it-backend/functions/PutNote/handler.js
+++ backend/just-post-it-backend/functions/PutNote/handler.js
@@ -3,35 +3,39 @@ const {db} = require('../../services/db')
 
 exports.hello = async (event) => {
 
-  const id = event.pathParameters.id;
-  const body = JSON.parse(event.body);
-  
-  const checkParams = {
-    TableName: 'NoteManager',
-    Key: {
-      id: id
-    }
-  }
-
-  const prevNote = await db.get(checkParams)
-  const username = prevNote.Item.username
-  const createdAt = prevNote.Item.createdAt
-  const date = new Date().toISOString().split('T')[0];
-
-  if (!prevNote.Item) {
-    return sendError(404, "Note not found")
-  } else {
-    const data = await db.put({
+  try {
+    const id = event.pathParameters.id;
+    const body = JSON.parse(event.body);
+    
+    const checkParams = {
       TableName: 'NoteManager',
-      Item: {
-        id: id,
-        username: username,
-        text: body.text,
-        createdAt: createdAt,
-        updatedAt: date
+      Key: {
+        id: id
       }
-    })
+    }
+
+    const prevNote = await db.get(checkParams)
+
+    if (!prevNote.Item) {
+      return sendError(404, "Note not found")
+    } else {
+      const username = prevNote.Item.username
+      const createdAt = prevNote.Item.createdAt
+      const date = new Date().toISOString().split('T')[0];
+      const data = await db.put({
+        TableName: 'NoteManager',
+        Item: {
+          id: id,
+          username: username,
+          text: body.text,
+          createdAt: createdAt,
+          updatedAt: date
+        }
+      })
 
     return sendResponse({message: "Updated!"})}
+  } catch (error) {
+    return sendError(500, error.message)
+  }
 
   };
