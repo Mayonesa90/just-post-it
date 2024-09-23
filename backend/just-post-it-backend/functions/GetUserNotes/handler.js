219d87dd7f38c8ed6db5af2a591f2439f307b558
Maya Arzapalo Björklund <mayonaas@hotmail.com>
2024-09-23 21:55:11 +0200

    added a get user notes function that get notes from a specifik user based on username

--- backend/just-post-it-backend/functions/GetUserNotes/handler.js
+++ backend/just-post-it-backend/functions/GetUserNotes/handler.js
@@ -1,9 +1,32 @@
+const {sendResponse, sendError} = require('../../responses/handler')
+const {db} = require('../../services/db') 
+
 exports.hello = async (event) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "Go Serverless v4! Your function executed successfully!",
-      }),
-    };
+
+  try {
+    const username = event.pathParameters.user;
+    console.log(username);
+    
+    const checkParams = {
+      TableName: 'NoteManager',
+      FilterExpression: "username = :username",
+      ExpressionAttributeValues: {
+        ":username": username
+      }
+    }
+
+    const userNotes = await db.scan(checkParams)
+
+    if (!userNotes) {
+      return sendError(404, "User not found")
+    } 
+   
+    return sendResponse(userNotes.Items)
+    
+    
+
+  } catch (error) {
+    return sendError(500, error.message)
+  }
+
   };
-  
