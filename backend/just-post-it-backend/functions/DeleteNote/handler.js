97894b56906d54e73ff156d9ba9f5597bd55f6f6
Maya Arzapalo Björklund <mayonaas@hotmail.com>
2024-09-23 20:13:43 +0200

    added delete function to delete note from database

--- backend/just-post-it-backend/functions/DeleteNote/handler.js
+++ backend/just-post-it-backend/functions/DeleteNote/handler.js
@@ -1,9 +1,33 @@
+const {sendResponse, sendError} = require('../../responses/handler')
+const {db} = require('../../services/db') 
+
 exports.hello = async (event) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "Go Serverless v4! Your function executed successfully!",
-      }),
-    };
+
+  try {
+    const id = event.pathParameters.id;
+    
+    const checkParams = {
+      TableName: 'NoteManager',
+      Key: {
+        id: id
+      }
+    }
+
+    const prevNote = await db.get(checkParams)
+
+    if (!prevNote.Item) {
+      return sendError(404, "Note not found")
+    } else {
+      await db.delete({
+        TableName: 'NoteManager',
+        Key: {
+          id
+        }
+      })
+
+    return sendResponse({message: "Note deleted!"})}
+  } catch (error) {
+    return sendError(500, error.message)
+  }
+
   };
-  
