a4be0a9064547ff90a1cee4dfb7f973b854b2b30
Maya Arzapalo Björklund <mayonaas@hotmail.com>
2024-09-23 18:15:52 +0200

    added post note function to add note to database"

--- backend/just-post-it-backend/functions/PostNote/handler.js
+++ backend/just-post-it-backend/functions/PostNote/handler.js
@@ -1,9 +1,25 @@
+const {sendResponse, sendError} = require('../../responses/handler')
+const {db} = require('../../services/db') 
+const {v4: uuidv4} = require('uuid')
+
 exports.hello = async (event) => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "Go Serverless v4! Your function executed successfully!",
-      }),
-    };
+
+  try {
+    const body = JSON.parse(event.body)
+    const id = uuidv4()
+    const date = new Date().toISOString()
+    const data = await db.put({
+      TableName: 'NoteManager',
+      Item: {
+        id: id,
+        username: body.username,
+        text: body.text,
+        createAt: date
+      }
+    })
+    return sendResponse({message: "Posted!"})
+  } catch (error) {
+    return sendError(500, error.message)
+  }
   };
   
