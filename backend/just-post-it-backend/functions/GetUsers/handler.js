0a70c7afe3ef9ca9759949f59e445c4ec4d3e174
Maya Arzapalo Björklund <mayonaas@hotmail.com>
2024-09-24 09:35:51 +0200

    added get users function

---
+++ backend/just-post-it-backend/functions/GetUsers/handler.js
@@ -0,0 +1,26 @@
+const {sendResponse, sendError} = require('../../responses/handler')
+const {db} = require('../../services/db') 
+
+exports.hello = async (event) => {
+
+  try {
+    
+    const checkParams = {
+      TableName: 'NoteManager',
+    }
+
+    const data = await db.scan(checkParams)
+    
+    if (data.Items.length === 0 ) {
+      return sendError(404, "No users found")
+    } 
+
+    const users = data.Items.map((user) => user.username)
+
+  return sendResponse(users)
+
+  } catch (error) {
+    return sendError(500, error.message)
+  }
+
+  };
