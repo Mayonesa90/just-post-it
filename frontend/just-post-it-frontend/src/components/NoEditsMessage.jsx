6db85412ee3e71fba760abddaa9890fc2cf76ee4
Maya Arzapalo Björklund <mayonaas@hotmail.com>
2024-09-25 15:17:36 +0200

    editing now working!! and added message when no editing has been done

---
+++ frontend/just-post-it-frontend/src/components/NoEditsMessage.jsx
@@ -0,0 +1,10 @@
+export default function NoEditsMessage () {
+        
+    return (
+      <div className=' font-PassionOne bg-orange-500 w-fit p-2 absolute top-24 place-self-center'>
+        <p className=" text-xl text-slate-50 tracking-wide">
+          No changes made
+        </p>
+      </div>
+    )
+  }
--- frontend/just-post-it-frontend/src/pages/EditNote.jsx
+++ frontend/just-post-it-frontend/src/pages/EditNote.jsx
@@ -2,7 +2,7 @@ import { useEffect,  useRef, useState  } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link, useLocation } from "react-router-dom"
 import UserIcon from '../assets/user-icon.svg'
 import NotePostedMessage from '../components/NotePostedMessage'
-import MustFillTextField from '../components/MustFillTextField'
+import NoEditsMessage from '../components/NoEditsMessage'
 
 
 export default function EditNote() {
@@ -10,6 +10,12 @@ export default function EditNote() {
     const location = useLocation()
     const {id} = location.state
     const [note, setNote] = useState('')
+    const [placeholder, setPlaceholder] = useState('')
+    const [text, setText] = useState('')
+    const [formData, setFormData] = useState({
+        text: "",
+        username: ""
+      })
 
     useEffect(() => {
         const fetchNote = async () => {
@@ -18,7 +24,6 @@ export default function EditNote() {
                 const data = await res.json()
                 
                 setNote(data.data[0])
-                // setPlaceholder(data.data[0].username)
             } catch (error) {
                 setNote('No users found')
                 console.log(error)
@@ -26,109 +31,63 @@ export default function EditNote() {
         }
         fetchNote()
     }, [])
+ 
     
-    
-    //Placeholder functions
-    const [placeholder, setPlaceholder] = useState('')
-    const [text, setText] = useState('')
-
     useEffect(() => {
         if(note){
             setPlaceholder(note.username)
             setText(note.text)
+            setFormData({
+                text: note.text,
+                username: note.username
+            })
         }
     }, [note])
 
-    console.log('placeholder: ', placeholder);
-    
-    //When clicked 
-    function handlePlaceholderClick(){
-        setPlaceholder("")
-    }
-
-    function handlePlaceholderOutsideClick(){
-        if (note) {
-            setPlaceholder(note.username)
-        }
-    }
-
-    const useOutsideClick = (callback) => {
-        const ref = useRef();
-    
-        useEffect(() => {
-          const handleClickOutside = (event) => {
-            if (ref.current && !ref.current.contains(event.target)) {
-              callback();
-            }
-          };
-    
-          document.addEventListener("mousedown", handleClickOutside);
-    
-          return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-          };
-        }, [ref]);
-    
-        return ref;
-      };
-    
-      const ref = useOutsideClick(handlePlaceholderOutsideClick);
-      
-
 
       //Form functions
-      const [formData, setFormData] = useState({
-        text: "",
-        username: ""
-      })
-
       const [textFilled, setTextFilled] = useState(false)
       const [usernameFilled, setUsernameFilled] = useState(false)
       const [showMessage, setShowMessage] = useState(false)
       const [notePosted, setNotePosted] = useState(false)
+      const [newText, setNewText] = useState(false)
+      const [newUsername, setNewUsername] = useState(false)
       
       const handleInputChange = (event) => {
         
         const { name, value } = event.target;
-
-        setFormData({
-          ...formData,
-          [name]: value
-        });
   
-        if(name === "text" && value.length > 0){
-            setTextFilled(true)
-            if(usernameFilled){
-              setShowMessage(false)
-            } else {
+        if(name === "text" && value.length > 0 && value !== note.text){
+              setNewText(true)
+              setFormData({
+              ...formData,
+              [name]: value
+            });
+            
+            if(!newText){
               setShowMessage(true)
+            } else {
+              setShowMessage(false)
             }
-        } else if (name === "text" && value.length === 0){
-            setTextFilled(false)
-            setShowMessage(true)
+            
+        } 
+
+        if(name === "username" && value.length > 0 && value !== note.username){
+              setNewUsername(true)
+              setFormData({
+              ...formData,
+              [name]: value
+            });
         }
         
-
-        if(name === "username" && value.length > 0){
-          setUsernameFilled(true)
-          if (textFilled){
-            setShowMessage(false)
-          } else {
-            setShowMessage(true)
-          }
-        } else if (name === 'username' && value.length === 0) {
-          setUsernameFilled(false)
-          setShowMessage(true)
-        }
-
       };
 
       const addNote = async (event) => {
         event.preventDefault();
 
-        if(textFilled && usernameFilled){
+        if(newText || newUsername){ 
           try {
-            const response = await fetch('https://4lrhfx9au9.execute-api.eu-north-1.amazonaws.com/notes/add-note', {
+            const response = await fetch(`https://4lrhfx9au9.execute-api.eu-north-1.amazonaws.com/notes/${id}`, {
               method: 'PUT',
               headers: {
                 'Content-Type': 'application/json'
@@ -136,17 +95,17 @@ export default function EditNote() {
               body: JSON.stringify(formData)
             });
             if (response.ok) {
-              console.log('Note added successfully');
+              console.log('Note edited successfully');
               setNotePosted(true)
             } else {
-              console.error('Failed to add note');
+              console.error('Failed to edit note');
             }
           } catch (error) {
-            console.error('Error adding note:', error);
+            console.error('Error editing note:', error);
           }
         } else {
           setShowMessage(true)
-          console.log("Please fill in the missing field/s before submitting.")
+          console.log("No changes made")
         }
       }
    
@@ -162,8 +121,8 @@ export default function EditNote() {
             <section className="grid max-w-96 mx-auto bg-yellow-200 font-GochiHand pt-6 ">
                 <textarea 
                     name="text" 
-                    id="text" 
-                    placeholder={text}
+                    id="text"
+                    defaultValue={note.text}
                     onChange={handleInputChange}
                     className="bg-yellow-200 min-h-96 px-9 tracking-wider"
                 >
@@ -172,12 +131,10 @@ export default function EditNote() {
                     <img src={UserIcon} alt="" className=' mr-2' />
                     <input 
                       size={15} 
-                      ref={ref} 
-                      onClick={handlePlaceholderClick} 
                       type="text"
                       id="username"
                       name="username" 
-                      placeholder={placeholder}
+                      defaultValue={note.username}
                       onChange={handleInputChange} 
                       className="max-w-52 bg-transparent"/>
                 </div>
@@ -193,7 +150,7 @@ export default function EditNote() {
               }
 
             </form>
-            {showMessage && <MustFillTextField />}
+            {showMessage && <NoEditsMessage />}
             {notePosted && <NotePostedMessage />}
         </div>
     )
