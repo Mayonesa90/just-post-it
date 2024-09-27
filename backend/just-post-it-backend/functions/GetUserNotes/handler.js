31e217fa182a0c593bd0dddfe62b687fcea1b4c9
Maya Arzapalo Björklund <mayonaas@hotmail.com>
2024-09-27 13:09:14 +0200

    adjusted GetUserNotes to unescape

--- backend/just-post-it-backend/functions/GetUserNotes/handler.js
+++ backend/just-post-it-backend/functions/GetUserNotes/handler.js
@@ -1,5 +1,6 @@
 const {sendResponse, sendError} = require('../../responses/handler')
 const {db} = require('../../services/db') 
+const validator = require('validator')
 
 exports.hello = async (event) => {
 
@@ -15,11 +16,24 @@ exports.hello = async (event) => {
     }
 
     const userNotes = await db.scan(checkParams)
+
+
     
     if (userNotes.Items.length === 0 ) {
       return sendError(404, "User not found")
     } 
-    const sortedData = userNotes.Items.sort((a, b) => {
+
+    //Unescape the escaped characters before returning the response
+    const unescapedData = userNotes.Items.map((item) => ({
+      ...item,
+      username: validator.unescape(item.username),
+      text: validator.unescape(item.text),
+    }));
+
+    console.log('unescapedData', unescapedData);
+    
+
+    const sortedData = unescapedData.Items.sort((a, b) => {
       const dateA = new Date(a.createdAt);
       const dateB = new Date(b.createdAt);
       return dateB - dateA;  
