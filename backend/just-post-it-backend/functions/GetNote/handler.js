3655e16d7611ae9ab7906049c9a0acd91b48023d
Maya Arzapalo Björklund <mayonaas@hotmail.com>
2024-09-27 12:52:17 +0200

    added unescaping to get note

--- backend/just-post-it-backend/functions/GetNote/handler.js
+++ backend/just-post-it-backend/functions/GetNote/handler.js
@@ -1,5 +1,6 @@
 const {sendResponse, sendError} = require('../../responses/handler')
 const {db} = require('../../services/db') 
+const validator = require('validator')
 
 exports.hello = async (event) => {
 
@@ -18,8 +19,21 @@ exports.hello = async (event) => {
         return sendError(404, "Can't find any note with that id")
         }
         
-        return sendResponse(data.Items)
+        console.log(data.Items[0].text);
+        
+        //Unescape the escaped characters before returning the response
+    const unescapedData = {
+        ...data.Items[0],
+        username: validator.unescape(data.Items[0].username),
+        text: validator.unescape(data.Items[0].text),
+      };
+      console.log('unescapedData', unescapedData);
+      console.log('data.Items[0]', data.Items[0]);
+      
+    //   return sendResponse(data.Items)
 
+  
+      return sendResponse(unescapedData);
   } catch (error) {
         return sendError(500, error.message)
   }
